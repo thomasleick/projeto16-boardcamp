09d87ee3133fe4c4596597b02b33234179fe48a4
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-05-15 00:28:28 -0300

    image required on gameSchema

--- src/schemas/gameSchema.js
+++ src/schemas/gameSchema.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 const gameSchema = Joi.object({
   name: Joi.string().required(),
-  image: Joi.string(),
+  image: Joi.string().required(),
   stockTotal: Joi.number().integer().min(1).required(),
   pricePerDay: Joi.number().integer().min(1).required(),
 });
